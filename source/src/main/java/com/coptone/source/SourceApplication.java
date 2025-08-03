f5babafe9869680fce18ef497e076c4820cedd4b
Coptone <727577622@qq.com>
2025-08-03 13:55:41 +0800

    主要更新/main maintenance: 上传了application类

---
+++ source/src/main/java/com/coptone/source/SourceApplication.java
@@ -0,0 +1,15 @@
+package com.coptone.source;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.coptone.source.*")
+@MapperScan("com.coptone.source.dao")
+public class SourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceApplication.class, args);
+    }
+
+}
